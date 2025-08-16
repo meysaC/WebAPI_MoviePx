702cfcb4017d40a118c6d6522b55bde992f1ff96
Coddedx <86998728+Coddedx@users.noreply.github.com>
2025-08-16 18:36:44 +0300

    movie-13/restful api standart fix

---
+++ api/Dtos/User/Watched/CreateWatchedDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Dtos.User.Watched
+{
+    public class CreateWatchedDto
+    {
+        public int MovieId { get; set; }
+    }
+}
